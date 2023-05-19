c83e78dcba8a0f78cf3768022ccedebc3d1be1ad
yatoyun <kaneko.yuta0813@gmail.com>
2023-05-19 21:34:44 +0900

    Make Outputs Class for easier reading. And changed the methods and variables of SolveRoutes from static

--- src/main/java/ie03/phase2/task5/GetMinimumRoute.java
+++ src/main/java/ie03/phase2/task5/GetMinimumRoute.java
@@ -68,23 +68,19 @@ public class GetMinimumRoute {
         if (npos.x < 0 || npos.x >= w || npos.y < 0 || npos.y >= h) {
             return false;
         }
-
         // check if visited
         if (visited[npos.x][npos.y]) {
             return false;
         }
-
         // check if wall
         if (grid[npos.x][npos.y] == Integer.MAX_VALUE) {
             return false;
         }
-
         // main logic
         return (totaldist[npos.x][npos.y] > totaldist[cpos.x][cpos.y] + grid[npos.x][npos.y]);
     }
 
     public int solveDist(Point cpos, Point dpos) {
-
         int[][] totaldist = solveTotalDist(cpos, dpos);
         return totaldist[dpos.x][dpos.y];
     }
