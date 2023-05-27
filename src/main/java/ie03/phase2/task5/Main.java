1e0d4535be6a41315f4b6b5c4c4876b069566469
gyopiter <22850165+gyopiter@users.noreply.github.com>
2023-05-27 12:54:14 +0900

    refactored code on task5

--- src/main/java/ie03/phase2/task5/Main.java
+++ src/main/java/ie03/phase2/task5/Main.java
@@ -1,13 +1,5 @@
 package ie03.phase2.task5;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.Map.Entry;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         // input 01
@@ -15,6 +7,6 @@ public class Main {
         grid.warehouseSetup();
 
         // input 02 and solve
-        Outputs.inputSecond(grid.w, grid.h, grid.grid, grid.shelves);
+        Outputs.inputSecond(grid);
     }
 }
