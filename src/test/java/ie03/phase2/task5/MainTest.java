2629a4642b8e7f6f38b2cd773e60c45046e18320
gyopiter <22850165+gyopiter@users.noreply.github.com>
2023-06-21 16:12:25 +0900

    fixed code

--- src/test/java/ie03/phase2/task5/MainTest.java
+++ src/test/java/ie03/phase2/task5/MainTest.java
@@ -1,20 +1,60 @@
 package ie03.phase2.task5;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.DynamicTest;
-
-import ie03.*;
 import org.junit.jupiter.api.TestFactory;
 
 import java.io.IOException;
 import java.util.*;
+
+import ie03.*;
+
+import ie03.phase2.task5.generator.TestCaseGenerator;
+
 public class MainTest extends TestRunner implements TestInterface {
 
     @TestFactory
     public Collection<DynamicTest> generatedTest() throws Exception {
-        return new ArrayList<>();
+
+
+        TestCaseWriter tcWriter = new TestCaseWriter("/generated_testcases/phase2/task5");
+
+        final int numTestCases = 25;
+
+        List<DynamicTest> tests = new ArrayList<>();
+
+        Main main = new Main();
+
+        for (int i = 1; i <= numTestCases; i++) {
+
+            int currentI = i;
+            tests.add(DynamicTest.dynamicTest("Generated Test " + i, () -> {
+                TestCaseGenerator generator = new TestCaseGenerator();
+                generator.setTestText(currentI);
+                generator.runGenerator();
+
+                String input = generator.getIntputText();
+                String outputActual = execute(input, main);
+                String outputExpected = generator.getOutputText();
+
+                // write input to file
+                tcWriter.writeTestCase(input, "testcase_" + currentI + "_in.txt");
+                // write output to file
+                tcWriter.writeTestCase(outputActual, "testcase_" + currentI + "_out_actual.txt");
+                // write confirmation to file
+                tcWriter.writeTestCase(outputExpected, "testcase_" + currentI + "_out_expect.txt");
+
+                // print
+                System.err.println("[Input] \n" + input);
+                System.err.println("[Actual Output] \n" + outputActual);
+                System.err.println("[Expected Output] \n" + outputExpected);
+
+                assertEquals(outputExpected, outputActual);
+            }));
+        }
+        return tests;
     }
+
 
     @TestFactory
     public Collection<DynamicTest> exampleTest() throws IOException {
