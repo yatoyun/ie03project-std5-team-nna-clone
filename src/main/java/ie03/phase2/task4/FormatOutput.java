b573809d127bc89424e9dc90cf34f160558cdde2
gyopiter <22850165+gyopiter@users.noreply.github.com>
2023-07-22 08:45:29 +0900

    refactored task4

--- src/main/java/ie03/phase2/task4/FormatOutput.java
+++
@@ -1,9 +0,0 @@
-package ie03.phase2.task4;
-
-public class FormatOutput {
-    String deleteColon(String str) {
-        String output = str.replace(",", "").replace("(", "").replace(")", "");
-
-        return output;
-    }
-}
--- src/main/java/ie03/phase2/task4/Main.java
+++ src/main/java/ie03/phase2/task4/Main.java
@@ -1,5 +1,7 @@
 package ie03.phase2.task4;
 
+import ie03.phase1.task2.ProductsData;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -8,36 +10,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // Generate combinations
-        GenerateCombination gc = new GenerateCombination();
-        // Generate purchases list
-        List<Map.Entry<PurchasePair, Integer>> sortedList = null;
 
         long startTime = System.nanoTime();
 
         int N = Integer.parseInt(sc.next());
 
+        ProductsData productsData = new ProductsData();
+
         // Read strings from standard input
         for (int i = 0; i < N; i++) {
             int m = Integer.parseInt(sc.next());
             List<String> purchases = new ArrayList<>();
-
             for (int j = 0; j < m; j++) {
                 String items = sc.next();
                 purchases.add(items);
             }
-            sortedList = gc.sortCombination(gc.generateAndStoreCombinations(purchases));
+            productsData.addCustomer((ArrayList<String>) purchases);
         }
+
+        productsData.setPurchasePair();
+        productsData.sortPurchasePair();
+
 
         int q = Integer.parseInt(sc.next());
         for (int i = 0; i < q; i++) {
             int first = Integer.parseInt(sc.next());
             int last = Integer.parseInt(sc.next());
-            gc.printCombinations(sortedList, first, last);
+            for (int j = first - 1; j < last; j++) {
+                Map.Entry<String, Integer> entry = productsData.sortedPurchasePair.get(j);
+                System.out.println(entry.getValue() + " " + entry.getKey());
+            }
         }
 
         long endTime = System.nanoTime();
-        long result = (long) ((endTime - startTime) / 1e9);
-        System.err.println(result + "sec");
+        long result = (long) ((endTime - startTime) / 1e6);
+        System.err.println(result + " ms");
     }
 }
